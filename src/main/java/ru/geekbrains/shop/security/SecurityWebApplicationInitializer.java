3b4cf1619565077cc12dd0385ffd7c2879ba8671
Strelok90 <73306594+Strelok90@users.noreply.github.com>
2021-06-01 16:53:33 +0300

    Revert "Практическое задание к 2 уроку"

--- src/main/java/ru/geekbrains/shop/security/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.shop.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
